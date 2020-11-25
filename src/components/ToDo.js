658fb9b57874aeee62fbbf7bf9f0971826d44f31
Michael Walker <Michael.e.walker.87@gmail.com>
2020-11-25 11:44:49 -0700

    Add the crossout line

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,11 +1,22 @@
 // components/ToDo.js
-
+import {toggleToDo} from '../actions/index.js'
 import React from 'react';
+import { connect } from 'react-redux';
+ 
+const ToDo = ({ id, todo, completed, toggleToDo}) => {
 
-const ToDo = ({ id, todo, completed }) => {
   return (
-    <li>{todo}</li>
+    <li 
+      className={completed ? "completed" : "not-completed"}
+      onClick={() => toggleToDo(id)}
+    >
+      {todo} 
+    </li>
   )
 }
 
-export default ToDo;
+const mapDispatchToProps = dispatch => ({
+  toggleToDo: id => dispatch( toggleToDo(id) )
+})
+
+export default connect(null, mapDispatchToProps)(ToDo);
